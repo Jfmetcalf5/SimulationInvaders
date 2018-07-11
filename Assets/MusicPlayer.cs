be14ecb49b1850d962f81ecbc0fa71dc5d8fea37
Jfmetcalf5 <Jfmetcalf5@gmail.com>
2018-07-11 12:24:40 -0600

    got the waypoints and ship in front of the camera

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        Invoke("loadFirstScene", 2f);
+    }
+    
+    void loadFirstScene() {
+        SceneManager.LoadScene(1);
+    }
+}
